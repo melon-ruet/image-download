8fb10b1a23c68dc52329e3d5a573add89cee3c17
Mahabub Melon <melon.ruet@gmail.com>
2019-02-26 11:04:56 +0600

    argv added

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import urllib.request
 
 download_dir = 'images'
@@ -11,3 +12,7 @@ def download_images(image_file):
     with open(image_file, 'r') as file:
         for line in file:
             urllib.request.urlretrieve(line, download_dir + os.sep + line.split('/')[-1] + '.jpg')
+
+
+if __name__ == '__main__':
+    download_images(sys.argv[-1])
