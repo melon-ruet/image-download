870b44db7520c7669463021e7553d3240c96d364
Mahabub Melon <melon.ruet@gmail.com>
2019-02-26 11:01:33 +0600

    download images

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import os
+import urllib.request
 
 download_dir = 'images'
 
@@ -6,3 +7,7 @@ download_dir = 'images'
 def download_images(image_file):
     if not os.path.exists(download_dir):
         os.mkdir(download_dir)
+
+    with open(image_file, 'r') as file:
+        for line in file:
+            urllib.request.urlretrieve(line, download_dir + os.sep + line.split('/')[-1] + '.jpg')
