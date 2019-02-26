cb99a9ef51e3ffc2c09d3bf71f8e311c441e4c8d
Mahabub Melon <melon.ruet@gmail.com>
2019-02-26 10:51:47 +0600

    test added

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+import os
+import shutil
 from unittest import TestCase
+
+from download import download_images, download_dir
 
 
 class DownloadTest(TestCase):
-    pass
+
+    def test_download_files(self):
+        test_image_file = 'images.txt'
+        self.assertEqual(os.path.exists(test_image_file), True, 'Test image file not found')
+        with open(test_image_file) as file:
+            count = len(file.readline())
+        download_images(test_image_file)
+        self.assertEqual(os.listdir(download_dir), count, 'All files download failed')
+        shutil.rmtree(download_dir)
